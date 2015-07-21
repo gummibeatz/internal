7acf46bea426d9124155a3f9a61e27e944c128af
mikekavouras <kavourasm@gmail.com>
2015-07-21 00:19:12 -0400

    google login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users,
+    skip: [:registrations, :passwords],
+    controllers: { :omniauth_callbacks => "users#omniauth_callbacks" }
   root 'welcome#index'
 end
