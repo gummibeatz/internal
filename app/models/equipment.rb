1935def92dc56954bed4879c5eb7a9897de2fead
mikekavouras <kavourasm@gmail.com>
2015-07-24 15:01:26 -0400

    annotate and enum for dev

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,2 +1,14 @@
 class Equipment < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: equipment
+#
+#  id           :integer          not null, primary key
+#  type         :integer
+#  description  :string
+#  reference_id :string           default("0")
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
--- test/models/equipment_test.rb
+++ test/models/equipment_test.rb
@@ -5,3 +5,15 @@ class EquipmentTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: equipment
+#
+#  id           :integer          not null, primary key
+#  type         :integer
+#  description  :string
+#  reference_id :string           default("0")
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
