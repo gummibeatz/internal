65ab6f882244231b190112c582fa32d728ed6f5a
mikekavouras <kavourasm@gmail.com>
2015-07-27 15:24:25 -0400

    remove a few comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+
   def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
 
       if @user.persisted?
@@ -10,4 +10,5 @@ class UsersController < ApplicationController
         redirect_to root_path
       end
   end
+
 end
