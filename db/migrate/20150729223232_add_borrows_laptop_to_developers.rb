67003e2233a6ee49a586433919fde7f27b6de2d5
mikekavouras <kavourasm@gmail.com>
2015-07-29 19:24:09 -0400

    oops

--- db/migrate/20150729223232_add_borrows_laptop_to_developers.rb
+++ db/migrate/20150729223232_add_borrows_laptop_to_developers.rb
@@ -2,7 +2,6 @@ class AddBorrowsLaptopToDevelopers < ActiveRecord::Migration
   def change
     add_column :developers, :borrows_laptop, :boolean
     add_column :developers, :race_ethnicity, :string
-    add_column :developers, :race_ethnicity, :string
     add_column :developers, :online_portfolio_url, :string
     add_column :developers, :graduate_college_or_university, :string
     add_column :developers, :graduate_concentration, :string
