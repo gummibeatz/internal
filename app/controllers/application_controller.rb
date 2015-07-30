376a2c8e57293801955e07abf01239d85dd1447a
mikekavouras <kavourasm@gmail.com>
2015-07-29 22:25:38 -0400

    better redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to root_url, :notice => 'You must be authenticated'
+    end
+  end
 end
