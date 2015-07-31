6f1fd180fedd61100e933fa21056f1628a1ef4a4
mikekavouras <kavourasm@gmail.com>
2015-07-31 11:47:17 -0400

    auth before api

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,7 @@
 class AttendancesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_filter :authenticate_user!, only: [:create]
+
   def create
     Attendance.create_from_google_form(params)
     head :ok
