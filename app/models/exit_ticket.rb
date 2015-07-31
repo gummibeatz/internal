a55f267192f279a6f77be06722958ba9e67e9234
mikekavouras <kavourasm@gmail.com>
2015-07-31 00:20:31 -0400

    lets try it

--- app/models/exit_ticket.rb
+++ app/models/exit_ticket.rb
@@ -5,11 +5,28 @@ class ExitTicket < ActiveRecord::Base
   def self.create_from_google_form(form_data)
     json = JSON.parse(form_data["ticket"])
     ticket = json["ticket"]
-    puts "*" * 80
-    puts ticket["overall_quality"]
-    puts ticket["difficulty"]
-    puts ticket["name"]
-    puts "*" * 80
+
+    ActiveRecord::Base.transaction do
+      developer = Developer.where(full_name: ticket["name"]).first
+      ticket = ExitTicket.new(
+        certainty: ticket["certainty"],
+        overall_quality: ticket["overall_quality"],
+        difficulty: ticket["difficulty"],
+        instructors_ability_to_communicate: ticket["instructors_ability_to_communicate"],
+        instructors_ability_to_stimulate: ticket["instructors_ability_to_stimulate"],
+        pace_and_speed: ticket["pace_and_speed"],
+        understanding: ticket["understanding"],
+        recall_information_from_previous_class: ticket["recall_information_from_previous_class"],
+        additional_comments: ticket["additional_comments"]
+      )
+      if ticket.save
+        developer.exit_tickets << ticket
+      else
+        return false
+      end
+    end
+
+    true
   end
 
 end
