2974d9daa68d201c8e190482248fd61b0b2f2129
mikekavouras <kavourasm@gmail.com>
2015-08-01 13:43:00 -0400

    update and submitted at

--- app/models/exit_ticket.rb
+++ app/models/exit_ticket.rb
@@ -35,22 +35,19 @@ class ExitTicket < ActiveRecord::Base
 
     ActiveRecord::Base.transaction do
       developer = Developer.where(full_name: tck["name"]).first
-      ticket = ExitTicket.new(
-        certainty: tck["certainty"],
-        overall_quality: tck["overall_quality"],
-        difficulty: tck["difficulty"],
-        instructors_ability_to_communicate: tck["instructors_ability_to_communicate"],
-        instructors_ability_to_stimulate: tck["instructors_ability_to_stimulate"],
-        pace_and_speed: tck["pace_and_speed"],
-        understanding: tck["understanding"],
-        recall_information_from_previous_class: tck["recall_information_from_previous_class"],
-        additional_comments: tck["additional_comments"]
-      )
-      if ticket.save
-        developer.exit_tickets << ticket
-      else
-        return false
-      end
+        submitted_at = DateTime.now.in_time_zone.midnight
+        ticket.delete("name")
+        if t = ExitTicket.where("developer_id = ? AND submitted_at = ?", developer.id, submitted_at).last
+          t.update_attributes(ticket)
+        else
+          ticket = ExitTicket.new(ticket)
+          ticket.submitted_at = submitted_at
+          if ticket.save
+            developer.exit_tickets << ticket
+          else
+            return false
+          end
+        end
     end
 
     true
