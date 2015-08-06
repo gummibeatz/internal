a660af389b868fedbbe5a972ab1b2a1037711d76
vkrish1 <veena.t.krish@gmail.com>
2015-08-06 17:40:12 -0400

    basic graphs up

--- app/assets/javascripts/exit_tickets/exit_ticket_graphs.js
+++ app/assets/javascripts/exit_tickets/exit_ticket_graphs.js
@@ -20,7 +20,8 @@ $.ajax({
 
 
 
-// Cause sometimes people are 1000000% confident
+// Clean up data; gets called in the ajax success. Then calls graphData(jsonData).
+//cause sometimes people are 1000000% confident
 function dataValidation(jsonData) {
 	jsonData.forEach(function(j) {
 		if ( parseInt(j.certainty) > 100 ) {
@@ -34,7 +35,7 @@ function dataValidation(jsonData) {
 }
 
 
-// d3/dimple function that gets called in the ajax sucess
+// D3/dimple function that gets called after data validation
 function graphData(jsonData) {
 	
 	
@@ -46,12 +47,29 @@ function graphData(jsonData) {
 		chart.setBounds(60,50,600,450); //play around w this
 		
 		var x = chart.addCategoryAxis("x", "submitted_at"),
-			y = chart.addMeasureAxis("y", "certainty");
+			y = chart.addMeasureAxis("y", "certainty"),
+			y2 = chart.addMeasureAxis(y, "score"),
+			y3 = chart.addMeasureAxis(y, "overall_quality"),
+			y4 = chart.addMeasureAxis(y, "difficulty"),
+			y5 = chart.addMeasureAxis(y, "instructors_ability_to_communicate"),
+			y6 = chart.addMeasureAxis(y, "instructors_ability_to_stimulate"),
+			y7 = chart.addMeasureAxis(y, "pace_and_speed"),
+			y8 = chart.addMeasureAxis(y, "understanding"),
+			y9 = chart.addMeasureAxis(y, "recall_information_from_previous_class");
 		
 		x.outputFormat = "%m/%d";
 		x.title = "Date";
-		y.title = "Certainty";
-		chart.addSeries([""], dimple.plot.line);
+		y.title = "Quantitative Questions";
+
+		chart.addSeries(["Quality"], dimple.plot.line, [x, y3]).aggregate = dimple.aggregateMethod.avg;
+		chart.addSeries(["Difficulty"], dimple.plot.line, [x, y4]).aggregate = dimple.aggregateMethod.avg;
+	chart.addSeries(["Communication"], dimple.plot.line, [x, y5]).aggregate = dimple.aggregateMethod.avg;
+	chart.addSeries(["Interest"], dimple.plot.line, [x, y6]).aggregate = dimple.aggregateMethod.avg;
+	chart.addSeries(["Speed"], dimple.plot.line, [x, y7]).aggregate = dimple.aggregateMethod.avg;
+	chart.addSeries(["Understanding"], dimple.plot.line, [x, y8]).aggregate = dimple.aggregateMethod.avg;
+	chart.addSeries(["Recall"], dimple.plot.line, [x, y9]).aggregate = dimple.aggregateMethod.avg;
+		
+	
 		chart.draw();
 	//}); 
 } 
