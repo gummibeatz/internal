472b48264a07796b287c379108c94d55ad1f808c
mikekavouras <kavourasm@gmail.com>
2015-08-14 16:32:11 -0400

    oops

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,31 +1,31 @@
 class Report
-	
-	attr_accessor :range
-	
-	# constructor
-	def initialize(start, finish)
-		@range = Range.new(start,finish)
-	end
-	
-	def exit_ticket_completion_rate
-		return ExitTicket.completion_rate_in_range(self.range)
-	end
-	
-	def exit_ticket_accuracy_rate
-		return ExitTicket.accuracy_rate_in_range(self.range)
-	end
-	
-	def attendance_present_rate
-		return Attendance.rate_in_range(self.range)
-	end
-	
-	def attendance_late_rate
-		return Attendance.percentage_late_in_range(self.range)
-	end
-	
-	def attendance_on_time_rate
-		return Attendance.percentage_on_time_in_range(self.range)
-	end
+
+  attr_accessor :range
+
+  # constructor
+  def initialize(start, finish)
+    @range = Range.new(start,finish)
+  end
+
+  def exit_ticket_completion_rate
+    return ExitTicket.completion_rate_in_range(self.range)
+  end
+
+  def exit_ticket_accuracy_rate
+    return ExitTicket.accuracy_rate_in_range(self.range)
+  end
+
+  def attendance_present_rate
+    return Attendance.rate_in_range(self.range)
+  end
+
+  def attendance_late_rate
+    return Attendance.percentage_late_in_range(self.range)
+  end
+
+  def attendance_on_time_rate
+    return Attendance.percentage_on_time_in_range(self.range)
+  end
 
 
 end
