6f06b21294aa8786d7737d2d99014b2081e5952c
vkrish1 <veena.t.krish@gmail.com>
2015-08-14 14:25:34 -0400

    works

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,46 +7,24 @@ class Report
 		@range = Range.new(start,finish)
 	end
 	
-	# below: mostly copied from exit_ticket.rb and attendance.rb
+	def exit_ticket_completion_rate
+		return ExitTicket.completion_rate_in_range(self.range)
+	end
 	
-	# percent submitted
-	def self.completion_rate_in_range
-		tickets = self.all_in_range(@range).technical
-    	timestamps = tickets.map(&:submitted_at).uniq
-    	attendance = Attendance.where("timestamp in (?)", timestamps).present
-    	return (tickets.count / attendance.count.to_f) unless count == 0
-    	return -1
-  	end
+	def exit_ticket_accuracy_rate
+		return ExitTicket.accuracy_rate_in_range(self.range)
+	end
 	
-	# percent accurate
-	def self.accuracy_rate_in_range
-		tickets = self.all_in_range(@range).technical
-    	return tickets.map(&:score).inject(0.0) { |sum, el| sum + (el || 0) }.to_f / tickets.count unless tickets.count == 0
-    	return -1
-  	end
+	def attendance_present_rate
+		return Attendance.rate_in_range(self.range)
+	end
+	
+	def attendance_late_rate
+		return Attendance.percentage_late_in_range(self.range)
+	end
+	
+	def attendance_on_time_rate
+		return Attendance.percentage_on_time_in_range(self.range)
+	end
 
-
-	# percent present
-	def self.rate_in_range
-    	all = self.all_in_range(@range)
-    	return all.present.count / all.count.to_f unless all.count == 0
-    	return -1
-  	end
-
-	# percent late
-  	def self.percentage_late_in_range
-		all = self.all_in_range(@range)
-    	present = all.present
-    	late = all.late
-    	late.count.to_f / present.count
-  	end
-
-	# percent on time
-  	def self.percentage_on_time_in_range
-		1 - self.percentage_late_in_range(@range)
-  	end
-	
-	
-	
-	
 end
