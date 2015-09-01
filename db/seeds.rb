dda5dd97a4c0eae743c2825deaca01503581123e
Linus Liang <linus.h.liang@gmail.com>
2015-09-01 16:08:35 -0400

    added seed developer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# creating fake developer
+dev = Developer.create(email: 'test@testing.com',
+                       github_username: 'test@testing.com')
