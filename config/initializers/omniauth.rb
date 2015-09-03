1332df2859d66b6165d15a00eb339a7dd00f33fb
mikekavouras <kavourasm@gmail.com>
2015-09-03 11:03:42 -0400

    we dont have ssl

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,21 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     :scope => "email, profile, plus.me",
     :image_size => {width: 200, height: 200},
-    :client_options => {
-        :ssl => {
-            :ca_file => "/usr/local/etc/openssl/certs/ca-bundle.crt",
-            :ca_path => "/etc/openssl/certs"
-        }
-    }
   }
 
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], {
-    :client_options => {
-      :ssl => {
-            :ca_file => "/usr/local/etc/openssl/certs/ca-bundle.crt",
-            :ca_path => "/etc/openssl/certs"
-        }
-    }
-  }
-  
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+
 end
