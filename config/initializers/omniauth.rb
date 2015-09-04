a84aa2f2613be324bd2d83e059054b36c9f2b5a2
mikekavouras <kavourasm@gmail.com>
2015-09-04 00:09:50 -0400

    no clue why this is broken

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     :scope => "email, profile, plus.me",
-    :image_size => {width: 200, height: 200},
+    :image_size => {width: 200, height: 200}
   }
 
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
