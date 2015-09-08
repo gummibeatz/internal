9220d71dc1d5033801eea46806a7da77d2060d9b
mikekavouras <kavourasm@gmail.com>
2015-09-08 00:05:49 -0400

    a bit more flexible

--- app/assets/javascripts/developers/developers/index.js
+++ app/assets/javascripts/developers/developers/index.js
@@ -3,24 +3,18 @@ $(document).ready(function() {
   developer = new Developer();
   developer.attendances.fetch({
     success: function(response) {
-      var attendances = developer.attendances.models;
-      // var html = Mustache.to_html($('#attendances-template').html(), {"attendances" : attendances});
-      var html = Mustache.to_html("<div>{{score}}</div>", attendances[0]);
-      var $attendances = $('#attendances');
-      $attendances.html(html);
-      /*
-      for (var i = 0; i < attendances.length; i++) {
-        $attendances.append(JSON.stringify(attendances[i]));
-      }
-      */
+      var attendances = { "attendances" : _.map(developer.attendances.models, function(model) {
+        return model.toJSON();
+      })};
+      Mustache._render('#attendances-template', '#attendances', attendances);
     }
   });
   developer.assessments.fetch({
     success: function(response) {
-      var assessments = _.map(developer.assessments.models, function(model) { return model.toJSON(); });
-      var html = Mustache.to_html($('#attendances-template').html(), {"assessments" : assessments});
-      var $assessments = $('#assessments');
-      $assessments.html(html);
+      var assessments = { "assessments" : _.map(developer.assessments.models, function(model) {
+        return model.toJSON();
+      })};
+      Mustache._render('#assessments-template', '#assessments', assessments);
     }
   });
 });
