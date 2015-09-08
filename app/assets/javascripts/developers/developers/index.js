bf65758bbb3912dd38dc8dc0ff8cc6bfa7f9c6f1
mikekavouras <kavourasm@gmail.com>
2015-09-07 23:40:47 -0400

    basic mustache implementation

--- app/assets/javascripts/developers/developers/index.js
+++ app/assets/javascripts/developers/developers/index.js
@@ -4,19 +4,23 @@ $(document).ready(function() {
   developer.attendances.fetch({
     success: function(response) {
       var attendances = developer.attendances.models;
+      // var html = Mustache.to_html($('#attendances-template').html(), {"attendances" : attendances});
+      var html = Mustache.to_html("<div>{{score}}</div>", attendances[0]);
       var $attendances = $('#attendances');
+      $attendances.html(html);
+      /*
       for (var i = 0; i < attendances.length; i++) {
         $attendances.append(JSON.stringify(attendances[i]));
       }
+      */
     }
   });
   developer.assessments.fetch({
     success: function(response) {
-      var assessments = developer.assessments.models;
+      var assessments = _.map(developer.assessments.models, function(model) { return model.toJSON(); });
+      var html = Mustache.to_html($('#attendances-template').html(), {"assessments" : assessments});
       var $assessments = $('#assessments');
-      for (var i = 0; i < assessments.length; i++) {
-        $assessments.append(JSON.stringify(assessments[i]));
-      }
+      $assessments.html(html);
     }
   });
 });
