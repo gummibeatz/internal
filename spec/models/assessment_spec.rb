0aafe166c40e21c6f8504fa60c9168c45f836853
Linus Liang <linus.h.liang@gmail.com>
2015-09-17 18:13:29 -0400

    DRYed up assessment spec methods

--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -20,59 +20,50 @@ RSpec.describe Assessment, type: :model do
   end
 
   describe "methods" do
-    let(:assessment) { {"assessment"=>"{\"type\":\"0\",\"github_url\":\"github.com\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"2\",\"due_at\":\"Sat, 15 Aug 2015 04:00:00 GMT\"}"} }
+    SAMPLE_ASSESSMENT = {"assessment"=>"{\"type\":\"0\",\"github_url\":\"github.com\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"2\",\"due_at\":\"Sat, 15 Aug 2015 04:00:00 GMT\"}"}
+    UPDATED_ASSESSMENT = {"assessment"=>"{\"type\":\"0\",\"github_url\":\"github.com\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"1\",\"due_at\":\"Sat, 15 Aug 2015 04:00:00 GMT\"}"}
+    NEW_ASSESSMENT = {"assessment"=>"{\"type\":\"0\",\"github_url\":\"http://github.com\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"1\",\"due_at\":\"Sat, 16 Aug 2015 04:00:00 GMT\"}"}
+
+    before(:each) do
+      @date = Date.parse(JSON.parse(SAMPLE_ASSESSMENT["assessment"])["due_at"])
+      @developer = create(:developer)
+      @cohort = create(:cohort)
+      @assignment = create(:assignment, github_url: "github.com")
+      @cohort.assignments << @assignment
+    end
 
     it "should save with valid json" do
-      date = Date.parse(JSON.parse(assessment["assessment"])["due_at"])
       expect {
-        developer = create(:developer)
-        cohort = create(:cohort)
-        assignment = create(:assignment, github_url: "github.com")
-        cohort.assignments << assignment
-        Assessment.create_from_google_form(assessment)
+        Assessment.create_from_google_form(SAMPLE_ASSESSMENT)
       }.to change(Assessment, :count).by(1)
     end
 
     it "should update an existing record if the developer and the due date are the same" do
-      updated_assessment = {"assessment"=>"{\"type\":\"0\",\"github_url\":\"github.com\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"1\",\"due_at\":\"Sat, 15 Aug 2015 04:00:00 GMT\"}"}
-      date = Date.parse(JSON.parse(assessment["assessment"])["due_at"])
-      developer = create(:developer)
-      cohort = create(:cohort)
-      assignment = create(:assignment, github_url: "github.com")
-      cohort.assignments << assignment
-      Assessment.create_from_google_form(assessment)
-      Assessment.create_from_google_form(updated_assessment)
+      Assessment.create_from_google_form(SAMPLE_ASSESSMENT)
+      Assessment.create_from_google_form(UPDATED_ASSESSMENT)
       expect(Assessment.last.score == 1).to be_truthy
     end
 
     it "should save assessments from different assignments as different records" do
-      new_assessment = {"assessment"=>"{\"type\":\"0\",\"github_url\":\"http://github.com\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"1\",\"due_at\":\"Sat, 16 Aug 2015 04:00:00 GMT\"}"}
-      date = Date.parse(JSON.parse(assessment["assessment"])["due_at"])
-      developer = create(:developer)
-      cohort = create(:cohort)
-      assignment = create(:assignment, github_url: "github.com")
       assignment2 = create(:assignment, github_url: "http://github.com")
-      cohort.assignments << assignment
-      cohort.assignments << assignment2
+      @cohort.assignments << assignment2
       expect {
-        Assessment.create_from_google_form(assessment)
-        Assessment.create_from_google_form(new_assessment)
+        Assessment.create_from_google_form(SAMPLE_ASSESSMENT)
+        Assessment.create_from_google_form(NEW_ASSESSMENT)
       }.to change(Assessment, :count).by(2)
     end
 
     it "should not create duplicate records for the same assessment" do
-      updated_assessment = {"assessment"=>"{\"type\":\"0\",\"http://github_url\":\"\",\"comments\":\"\",\"name\":\"Test Developer\",\"max_score\":\"3\",\"score\":\"1\",\"due_at\":\"Sat, 15 Aug 2015 04:00:00 GMT\"}"}
-      date = Date.parse(JSON.parse(assessment["assessment"])["due_at"])
-      create(:developer)
-      cohort = create(:cohort)
-      unit = create(:unit, start_at: date.yesterday, end_at: date.tomorrow)
-      cohort.units << unit
-      Assessment.create_from_google_form(assessment)
+      unit = create(:unit, start_at: @date.yesterday, end_at: @date.tomorrow)
+      @cohort.units << unit
+      Assessment.create_from_google_form(SAMPLE_ASSESSMENT)
       expect {
-        Assessment.create_from_google_form(updated_assessment)
+        Assessment.create_from_google_form(UPDATED_ASSESSMENT)
       }.to change(Assessment, :count).by(0)
     end
-  
+  end
+
+  describe "assessment emails" do
     it "should send_reports after creation" do
       expect {
         developer = create(:developer)
@@ -121,8 +112,7 @@ RSpec.describe Assessment, type: :model do
         assessment.score = 3
         assessment.save!
        }.to change(Notification, :count).by(1)
-    end
-    
+    end 
   end
 
 end
