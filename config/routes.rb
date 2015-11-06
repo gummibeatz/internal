23fafd410597e661e2a88db841f1ad152365fbfd
mikekavouras <kavourasm@gmail.com>
2015-11-06 17:43:28 -0500

    remove pledges and style the page a little better

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   scope path: '/admin', module: :admin do
 
     resources :units, only: [:show]
-    resources :pledges, only: [:index]
     resources :cohorts
     resources :reports, only: [:index]
     resources :exit_tickets do
