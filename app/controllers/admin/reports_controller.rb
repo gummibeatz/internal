d261a0b0337023b8a0eb28f8c7a5646f9b2788c5
Linus Liang <linus.h.liang@gmail.com>
2015-11-12 12:08:49 -0500

    admin reports for multiple cohorts

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -6,15 +6,22 @@ module Admin
 # do exit tickets by cohort
 
     def index
-      if params[:start_date].present? && params[:end_date].present?
+      if params[:start_date].present? && params[:end_date].present? && params[:cohort_version]
         range = Range.new(Date.parse(params[:start_date]).to_datetime, Date.parse(params[:end_date]).to_datetime)
+
+        developers = Cohort.includes(:developers).where(version: params[:cohort_version]).first.developers
+        #NEED TO FIX
+        @exit_tickets = ExitTicket.in_range(range).where(developer_id: developers) 
+        @attendance = Attendance.in_range(range).where(developer_id: developers)
         if request.xhr?
           render json: {
-            exit_tickets: ExitTicket.in_range(range),
-            attendance: Attendance.in_range(range)
+            exit_tickets: exit_tickets,
+            attendance: attendance
+       #     exit_tickets: ExitTicket.in_range(range),
+       #     attendance: Attendance.in_range(range)
           }
         else
-          @report = Report.new(range.begin, range.end)
+          @report = Report.new(range.begin, range.end, @exit_tickets, @attendance)
         end
       end
     end
